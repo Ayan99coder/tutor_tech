0fd803ac808d0e6fb1cbca9b43c4c3288aa037f1
Ayan javed <ayanjaved803@gmail.com>
2026-08-22 22:21:39 +0500

    commit

--- lib/features/session/viewmodal/session_notifier.dart
+++ lib/features/session/viewmodal/session_notifier.dart
@@ -2,27 +2,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tutor_tech/features/session/modal/session_repo.dart';
 import 'package:tutor_tech/features/session/provider/session_provider.dart';
 import 'package:tutor_tech/features/session/viewmodal/session_state.dart';
-enum SessionTargetType {
-  student,
-  tutor,
-  admin,
-}
+
+enum SessionTargetType { student, tutor, admin }
 
 class SessionTarget {
   final SessionTargetType type;
   final String? id;
 
-  const SessionTarget({
-    required this.type,
-    this.id,
-  });
+  const SessionTarget({required this.type, this.id});
 }
-class SessionNotifier extends FamilyNotifier<SessionState,SessionTarget>{
+
+class SessionNotifier extends FamilyNotifier<SessionState, SessionTarget> {
   late SessionRepository repo;
+
   @override
   SessionState build(SessionTarget target) {
     repo = ref.read(sessionRepo);
-   return SessionState();
+    return SessionState();
   }
-
 }
