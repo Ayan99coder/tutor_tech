daa4171364edc898cbb82ad8fa3c11e4de75e1d1
Ayan javed <ayanjaved803@gmail.com>
2026-09-18 22:02:48 +0500

    commit

---
+++ lib/features/tutor/view/show_groups_alert_box.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+class StudentGroupManagement extends ConsumerWidget {
+  const StudentGroupManagement({super.key});
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return AlertDialog(
+
+    );
+  }
+}
